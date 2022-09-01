1784ff9d7cca8ab03283cf3d239b6bfbf29841bc
Shuvo <shuvombm02@gmail.com>
2022-09-02 04:02:23 +0600

    Tags data addad

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const todos = require("./data/todos");
 const videos = require("./data/videos");
+const tags = require("./data/tags");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
 
 module.exports = {
   todos,
   videos,
+  tags,
   videos2,
   transactions,
 }
